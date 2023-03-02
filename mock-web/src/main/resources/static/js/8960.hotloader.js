0aa89bf1425c806ef476817a7e36f93bf375a618
chufusheng <68052607+chufusheng@users.noreply.github.com>
2023-03-02 16:38:06 +0800

    Delete mock-web/src/main/resources/static directory

--- mock-web/src/main/resources/static/js/8960.hotloader.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[8960],{8960:(e,n,i)=>{!function(e){"use strict";e.defineMode("diff",(function(){var e={"+":"positive","-":"negative","@":"meta"};return{token:function(n){var i=n.string.search(/[\t ]+?$/);if(!n.sol()||0===i)return n.skipToEnd(),("error "+(e[n.string.charAt(0)]||"")).replace(/ $/,"");var r=e[n.peek()]||n.skipToEnd();return-1===i?n.skipToEnd():n.pos=i,r}}})),e.defineMIME("text/x-diff","diff")}(i(25747))}}]);
-//# sourceMappingURL=8960.hotloader.js.map
